3b136f9c5e3e5bb941713ae9a732755ed4e22668
xensik <8714336+xensik@users.noreply.github.com>
2020-12-01 20:03:06 +0100

    update build

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,11 @@
+-- ========================
+-- Path
+-- ========================
 
 local _project_dir = path.getabsolute("src")
+
 function project_dir()
-	return path.getrelative(os.getcwd(), _project_dir)
+    return path.getrelative(os.getcwd(), _project_dir)
 end
 
 -- ========================
@@ -9,48 +13,47 @@ end
 -- ========================
 
 workspace "gsc-tool"
-	location "./build"
-	objdir "%{wks.location}/obj/%{cfg.buildcfg}/%{prj.name}"
-	targetdir "%{wks.location}/bin/%{cfg.buildcfg}"
-	targetname "%{prj.name}"
+location "./build"
+objdir "%{wks.location}/obj/%{cfg.buildcfg}/%{prj.name}"
+targetdir "%{wks.location}/bin/%{cfg.buildcfg}"
+targetname "%{prj.name}"
 
-	language "C++"
-	cppdialect "C++17"
+language "C++"
+cppdialect "C++17"
+architecture "x86_64"
 
-	architecture "x86_64"
-	
-	filter "action:vs*"
-		buildoptions "/Zc:__cplusplus"
-	filter{}
+filter "action:vs*"
+    buildoptions "/Zc:__cplusplus"
+filter{}
 
-	configurations { "debug", "release", }
+configurations { "debug", "release", }
 
-	symbols "On"
-	
-	configuration "release"
-		optimize "Full"
-		defines { "NDEBUG", "YY_NO_UNISTD_H"}
-	configuration{}
+symbols "On"
 
-	configuration "debug"
-		optimize "Debug"
-		defines { "DEBUG", "_DEBUG", "YY_NO_UNISTD_H" }
-	configuration {}
+configuration "release"
+    optimize "Full"
+    defines { "NDEBUG", "YY_NO_UNISTD_H"}
+configuration{}
 
-	startproject "gsc-tool"
+configuration "debug"
+    optimize "Debug"
+    defines { "DEBUG", "_DEBUG", "YY_NO_UNISTD_H" }
+configuration {}
+
+startproject "gsc-tool"
 
 -- ========================
 -- Projects
 -- ========================
 
-	include "src/tool.lua"
-	include "src/utils.lua"
-	include "src/IW5.lua"
-	include "src/IW6.lua"
-	include "src/SH1.lua"
+include "src/tool.lua"
+include "src/utils.lua"
+include "src/IW5.lua"
+include "src/IW6.lua"
+include "src/SH1.lua"
 
-	tool:project()
-	utils:project()
-	IW5:project()
-	IW6:project()
-	SH1:project()
+tool:project()
+utils:project()
+IW5:project()
+IW6:project()
+SH1:project()
