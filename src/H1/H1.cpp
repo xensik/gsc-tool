91d72c1e30acaa3b878fce7bc63c0f714fb0ff1a
xensik <8714336+xensik@users.noreply.github.com>
2020-12-19 15:16:55 +0100

    add MWR & MW2CR

---
+++ src/H1/H1.cpp
@@ -0,0 +1,21 @@
+// Copyright 2020 xensik. All rights reserved.
+//
+// Use of this source code is governed by a GNU GPLv3 license
+// that can be found in the LICENSE file.
+
+#include "H1.hpp"
+
+namespace H1
+{
+
+auto opcode_size(opcode id) -> std::uint32_t
+{
+    switch (id)
+    {
+    default:
+        LOG_ERROR("Couldn't resolve instruction size for 0x%hhX!", id);
+        return 0;
+    }
+}
+
+} // namespace H1
