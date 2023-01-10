652e21e2324f1b773ed2c75096ab6a26f45a63dc
Edo <edoardo.sanguineti222@gmail.com>
2023-01-10 11:49:05 +0000

    feature(github): add workflow (#40)
    feature(workflow): add GitHub actions

--- prebuild-macos.sh
+++ prebuild-macos.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 git submodule update --init --recursive
-./tools/macos/premake5 gmake
+./tools/macos/premake5 gmake2
--- premake5.lua
+++ premake5.lua
@@ -13,37 +13,69 @@ end
 dependencies.load()
 -------------------------------------------------
 workspace "gsc-tool"
+    startproject "xsk-gsc-tool"
     location "./build"
     objdir "%{wks.location}/obj/%{cfg.buildcfg}/%{prj.name}"
-    targetdir "%{wks.location}/bin/%{cfg.buildcfg}"
+    targetdir "%{wks.location}/bin/%{cfg.platform}/%{cfg.buildcfg}"
     targetname "%{prj.name}"
-    staticruntime "On"
-    language "C++"
-    cppdialect "C++20"
-    architecture "x86_64"
 
-    filter "action:vs*"
-        buildoptions "/bigobj"
-        buildoptions "/Zc:__cplusplus"
+    configurations { "debug", "release" }
+
+    if os.istarget("darwin") then
+        platforms { "x64", "arm64" }
+    else
+        platforms { "x86", "x64" }
+    end
+
+    filter "platforms:x86"
+        architecture "x86"
     filter {}
 
-    configurations { "Debug", "Release" }
+    filter "platforms:x64"
+        architecture "x86_64"
+    filter {}
+
+    filter "platforms:arm64"
+        architecture "ARM64"
+    filter {}
+
+    filter { "language:C++", "toolset:not msc*" }
+	    buildoptions "-std=c++20"
+    filter {}
+
+    filter "toolset:msc*"
+        buildoptions "/bigobj"
+        buildoptions "/Zc:__cplusplus"
+        buildoptions "/std:c++20"
+    filter {}
+
+    filter { "system:windows" }
+	    systemversion "latest"
+    filter {}
+
+    filter { "system:macosx" }
+	    systemversion "12.0"
+    filter {}
 
     symbols "On"
+    staticruntime "On"
     warnings "Extra"
 
-    filter "configurations:Release"
+    if os.istarget("linux") or os.istarget("darwin") then
+        buildoptions "-pthread"
+        linkoptions "-pthread"
+    end
+
+    filter "configurations:release"
         optimize "Full"
         defines { "NDEBUG" }
         flags { "FatalCompileWarnings" }
     filter {}
 
-    filter "configurations:Debug"
+    filter "configurations:debug"
         optimize "Debug"
         defines { "DEBUG", "_DEBUG" }
     filter {}
-
-    startproject "xsk-gsc-tool"
 
 project "xsk-gsc-tool"
     kind "ConsoleApp"
