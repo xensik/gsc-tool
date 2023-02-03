a19ddc38578af71b75d9de1c6c11f523a7a444c8
simon <simon@sm2.gg>
2023-02-03 17:59:27 +1100

    named most scriptfiles

--- src/iw7/iw7.hpp
+++ src/iw7/iw7.hpp
@@ -14,7 +14,7 @@ namespace xsk::gsc::iw7
 constexpr usize code_count = 153;
 constexpr usize func_count = 807;
 constexpr usize meth_count = 1500;
-constexpr usize token_count = 367;
+constexpr usize token_count = 682;
 constexpr u32 max_string_id = 0x13FCC;
 
 class context : public gsc::context
