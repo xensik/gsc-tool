d571d8e928cbe1f8a36b13b885f1350afcab8a3b
xensik <xensik@pm.me>
2024-11-01 12:05:23 +0100

    fix format

--- src/gsc/engine/s4_token.cpp
+++ src/gsc/engine/s4_token.cpp
@@ -10,10 +10,10 @@ namespace xsk::gsc::s4
 
 extern std::array<std::pair<u32, char const*>, token_count> const token_list
 {{
-	{ 0x07A4 , "scripts/engine/utility" },
-	{ 0x2AA9 , "br_quests_Enabled" },
-	{ 0x2AAA , "br_queststashlocations" },
-	{ 0x6D9A , "GetStructArray" },
+    { 0x07A4, "scripts/engine/utility" },
+    { 0x2AA9, "br_quests_enabled" },
+    { 0x2AAA, "br_queststashlocations" },
+    { 0x6D9A, "getstructarray" },
 }};
 
 } // namespace xsk::gsc::s4
