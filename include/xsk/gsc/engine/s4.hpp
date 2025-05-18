c508e5b10f5ecef9313ef49b0ec6b8ebf52e182f
HiNAtyu <125034349+ProjectHiNAtyu@users.noreply.github.com>
2025-05-18 17:58:45 +0900

    feat(s4): add new hashes (#256)
    Co-authored-by: xensik <xensik@pm.me>

--- include/xsk/gsc/engine/s4.hpp
+++ include/xsk/gsc/engine/s4.hpp
@@ -12,10 +12,10 @@ namespace xsk::gsc::s4
 {
 
 constexpr usize code_count = 190;
-constexpr usize func_count = 116;
-constexpr usize meth_count = 222;
-constexpr usize token_count = 282;
-constexpr u32 max_string_id = 0x110F3;
+constexpr usize func_count = 123;
+constexpr usize meth_count = 232;
+constexpr usize token_count = 335;
+constexpr u32 max_string_id = 0x110F4;
 
 struct context : public gsc::context
 {
