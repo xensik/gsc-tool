ce2c935badda54386851205d76497ffbff8bdfc6
Hadi77KSA <135042368+Hadi77KSA@users.noreply.github.com>
2026-03-27 19:59:10 +0300

    feat: t6 and t7 hash lists update (#266)

--- include/xsk/arc/engine/t7.hpp
+++ include/xsk/arc/engine/t7.hpp
@@ -12,7 +12,7 @@ namespace xsk::arc::t7
 {
 
 constexpr usize code_count = 16384;
-constexpr usize hash_count = 178806;
+constexpr usize hash_count = 197861;
 constexpr u64 header_magic = 0x1C000A0D43534780;
 
 struct context : public arc::context
