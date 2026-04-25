76df43c6baaf2ad5374b82d02e6c7e58c0e75932
mjkzy <mjkzyalt@gmail.com>
2026-04-24 20:13:36 -0500

    adjust hash for IW9/JUP (it was s4 oops)

--- include/xsk/utils/hash.hpp
+++ include/xsk/utils/hash.hpp
@@ -36,29 +36,24 @@ constexpr auto scr_dvar_hash(std::string_view str) -> u64
 
     switch (str.front())
     {
-        case 'a': case 'A': return fnv1a_64(str, 0xFC9B930CF7416D64ULL, iv);
-        case 'b': case 'B': return fnv1a_64(str, 0x9A88BEE10E2FB71CULL, iv);
-        case 'c': case 'C': return fnv1a_64(str, 0x193B2D4166F1E474ULL, iv);
-        case 'd': case 'D': return fnv1a_64(str, 0x90BBD97D817C3BB4ULL, iv);
-        case 'e': case 'E': return fnv1a_64(str, 0x4BF68A19BDA2F7BCULL, iv);
-        case 'f': case 'F': return fnv1a_64(str, 0x220E8473FBFE04E4ULL, iv);
-        case 'g': case 'G': return fnv1a_64(str, 0x85B013A4E70EDCACULL, iv);
-        case 'h': case 'H': return fnv1a_64(str, 0x35359AD030F0CECCULL, iv);
-        case 'i': case 'I': return fnv1a_64(str, 0xB742F8B10AE9E1F4ULL, iv);
-        case 'j': case 'J': return fnv1a_64(str, 0x629C7AB30DDD511CULL, iv);
-        case 'k': case 'K': return fnv1a_64(str, 0x7B50CE4016A19064ULL, iv);
-        case 'l': case 'L': return fnv1a_64(str, 0x827CCF83792891C4ULL, iv);
-        case 'm': case 'M': return fnv1a_64(str, 0xBDDB2ACB28358A7CULL, iv);
-        case 'n': case 'N': return fnv1a_64(str, 0x772E559AE9E22494ULL, iv);
-        case 'o': case 'O': return fnv1a_64(str, 0x0F579F12D8AF33CCULL, iv);
-        case 'p': case 'P': return fnv1a_64(str, 0x4EFF8082D8DFCB2CULL, iv);
-        case 'r': case 'R': return fnv1a_64(str, 0x68E7F378CBD4275CULL, iv);
-        case 's': case 'S': return fnv1a_64(str, 0x9EACF1B19DED9334ULL, iv);
-        case 't': case 'T': return fnv1a_64(str, 0xBD0C7686C08A04F4ULL, iv);
-        case 'u': case 'U': return fnv1a_64(str, 0x5219561CA516433CULL, iv);
-        case 'v': case 'V': return fnv1a_64(str, 0xBC6AD2D970767A44ULL, iv);
-        case 'w': case 'W': return fnv1a_64(str, 0x8355B5EA1EC0C1CCULL, iv);
-        case 'x': case 'X': return fnv1a_64(str, 0xF2C1E13850ECCEACULL, iv);
+        case 'a': case 'A': return fnv1a_64(str, 0xE4A68FF7D4912FD2ULL, iv);
+        case 'b': case 'B': return fnv1a_64(str, 0x35EC9BBDCBCE1C0EULL, iv);
+        case 'c': case 'C': return fnv1a_64(str, 0x0DA27710BE4CE51AULL, iv);
+        case 'd': case 'D': return fnv1a_64(str, 0x7238A5E2001AEB8EULL, iv);
+        case 'f': case 'F': return fnv1a_64(str, 0xAF25952FD142C84EULL, iv);
+        case 'g': case 'G': return fnv1a_64(str, 0xDF9B64790A1B6DB2ULL, iv);
+        case 'h': case 'H': return fnv1a_64(str, 0xF74A6B45EC63764EULL, iv);
+        case 'l': case 'L': return fnv1a_64(str, 0xBFF2A0737EB2B2BEULL, iv);
+        case 'm': case 'M': return fnv1a_64(str, 0xFBE5EB20A0DF848AULL, iv);
+        case 'o': case 'O': return fnv1a_64(str, 0xE81A3159054FF1F2ULL, iv);
+        case 'p': case 'P': return fnv1a_64(str, 0xA3BF3E5BFC4CB65EULL, iv);
+        case 'r': case 'R': return fnv1a_64(str, 0x0251C962D8AB28AEULL, iv);
+        case 's': case 'S': return fnv1a_64(str, 0x8913204B4A9FE29AULL, iv);
+        case 'u': case 'U': return fnv1a_64(str, 0x5221A5281FDEAC5AULL, iv);
+        case 'w': case 'W': return fnv1a_64(str, 0x62C0087A686DCC32ULL, iv);
+        case 'x': case 'X': return fnv1a_64(str, 0x220FCCF375CE78EEULL, iv);
+        case 'y': case 'Y': return fnv1a_64(str, 0xD368BF96F49C16B2ULL, iv);
+        case 'z': case 'Z': return fnv1a_64(str, 0x3070CCD75C99C51EULL, iv);
         default: return 0;
     }
 }
