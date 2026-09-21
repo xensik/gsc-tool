92d8b87c0073866f65d6b43c11add6f60621a60b
xensik <xensik@pm.me>
2026-09-21 16:53:25 +0200

    test: move assembler case onto the shared scaffolding
    Now that the branch sits on top of the test rework, the vector alignment
    case can use common.hpp like the rest of test/: TEMPLATE_TEST_CASE over
    the two big endian engines, types from fam<Ctx>, context from ctx<Ctx>()
    instead of building one per run.

--- test/assembler.cpp
+++ test/assembler.cpp
@@ -3,42 +3,49 @@
 // Use of this source code is governed by a GNU GPLv3 license
 // that can be found in the LICENSE file.
 
-#include "xsk/stdinc.hpp"
-#include "xsk/gsc/engine/iw5_xb.hpp"
-#include "xsk/gsc/engine/s1_ps.hpp"
+#include "common.hpp"
 #include <catch_amalgamated.hpp>
 
 namespace xsk::test
 {
 
-using namespace xsk::gsc;
+using xb_ctx = gsc::iw5_xb::context;
+using ps_ctx = gsc::s1_ps::context;
 
-// Big endian engines align the vector payload to 4 bytes, and stock bytecode
-// repeats the opcode in the last padding byte. Three OP_GetZero push the vector
-// off the boundary so the assembler has to pad: '<op> 00 00 <op>' then the floats.
-template <typename Ctx>
-auto assemble_padded_vector() -> void
+#define BIGENDIAN xb_ctx, ps_ctx
+
+// Big endian engines align the vector payload to 4 bytes and stock bytecode repeats
+// the opcode in the last padding byte. Three OP_GetZero push the vector off the
+// boundary, so the assembler has to pad: '<op> 00 00 <op>' and then the floats.
+
+TEMPLATE_TEST_CASE("assembler: big endian vector alignment", "[assembler][gsc]", BIGENDIAN)
 {
-    auto ctx = Ctx{ instance::server };
-    auto prog = assembly::make();
+    using assembly = typename fam<TestType>::assembly;
+    using function = typename fam<TestType>::function;
+    using instruction = typename fam<TestType>::instruction;
+    using opcode = typename fam<TestType>::opcode;
+
+    auto make_inst = [](opcode op, std::vector<std::string> data)
+    {
+        auto inst = instruction::make();
+        inst->opcode = op;
+        inst->data = std::move(data);
+        return inst;
+    };
+
     auto func = function::make();
     func->id = 1;
+    func->instructions.push_back(make_inst(opcode::OP_GetZero, {}));
+    func->instructions.push_back(make_inst(opcode::OP_GetZero, {}));
+    func->instructions.push_back(make_inst(opcode::OP_GetZero, {}));
+    func->instructions.push_back(make_inst(opcode::OP_GetVector, { "0", "0", "0" }));
 
-    for (auto i = 0; i < 3; ++i)
-    {
-        auto zero = instruction::make();
-        zero->opcode = opcode::OP_GetZero;
-        func->instructions.push_back(std::move(zero));
-    }
+    auto data = assembly::make();
+    data->functions.push_back(std::move(func));
 
-    auto vector = instruction::make();
-    vector->opcode = opcode::OP_GetVector;
-    vector->data = { "0", "0", "0" };
-    func->instructions.push_back(std::move(vector));
-    prog->functions.push_back(std::move(func));
-
-    auto const [script, stack, devmap] = ctx.assembler().assemble(*prog);
-    auto const id = ctx.opcode_id(opcode::OP_GetVector);
+    auto* c = ctx<TestType>();
+    auto const [script, stack, devmap] = c->assembler().assemble(*data);
+    auto const id = c->opcode_id(opcode::OP_GetVector);
     auto const payload = script.size - 12;
 
     REQUIRE(payload % 4 == 0);
@@ -46,24 +53,7 @@ auto assemble_padded_vector() -> void
     REQUIRE(script.data[payload - 3] == 0);
     REQUIRE(script.data[payload - 2] == 0);
     REQUIRE(script.data[payload - 1] == id);
-
-    for (auto i = payload; i < script.size; ++i)
-    {
-        REQUIRE(script.data[i] == 0);
-    }
-}
-
-TEST_CASE("assembler: big endian vector alignment", "[gsc][assembler]")
-{
-    SECTION("iw5 xb2")
-    {
-        assemble_padded_vector<iw5_xb::context>();
-    }
-
-    SECTION("s1 ps3")
-    {
-        assemble_padded_vector<s1_ps::context>();
-    }
+    REQUIRE(std::all_of(script.data + payload, script.data + script.size, [](u8 byte) { return byte == 0; }));
 }
 
 } // namespace xsk::test
